d13574e41aed721d9bddf99bb7eb57db68c8d4f5
Georgios Papadakis <ggppdk@users.noreply.github.com>
2016-04-26 01:12:39 +0300

    Updated jQuery Character Counter JS to v1.5.1
    Among other things fixes support for IE8, handles pasting text, makes use of maxlength attribute

--- js/jquery.charactercounter.js
+++ js/jquery.charactercounter.js
@@ -1,46 +1,57 @@
 /**
-* Character Counter v1.0
-* ======================
-*
-* Character Counter is a simple, Twitter style character counter.
-* 
-* https://github.com/dtisgodsson/jquery-character-counter
-*
-* @author Darren Taylor
-* @author Email: shout@darrenonthe.net
-* @author Twitter: darrentaytay
-* @author Website: http://darrenonthe.net
-*
-*/
+ * Character Counter v1.5.1
+ * ======================
+ *
+ * Character Counter is a simple, Twitter style character counter.
+ *
+ * https://github.com/dtisgodsson/jquery-character-counter
+ *
+ * @author Darren Taylor
+ * @author Email: shout@darrenonthe.net
+ * @author Twitter: darrentaytay
+ * @author Website: http://darrenonthe.net
+ *
+ */
 (function($) {
 
-    $.fn.characterCounter = function(options){
-      
+    $.fn.characterCounter = function(opts){
+
         var defaults = {
             exceeded: false,
+            counterSelector: false,
             limit: 150,
+            renderTotal: false,
             counterWrapper: 'span',
-            counterCssClass: 'help-block',
+            counterCssClass: 'counter',
             counterFormat: '%1',
             counterExceededCssClass: 'exceeded',
+            increaseCounting: false,
             onExceed: function(count) {},
             onDeceed: function(count) {},
-            customFields: {},
-        }; 
-            
-        var options = $.extend(defaults, options);
+            customFields: {}
+        };
+
+        var options = $.extend(defaults, opts);
 
         return this.each(function() {
-            $(this).after(generateCounter());
+            var html5len = $(this).attr('maxlength');
+            if (typeof html5len !== typeof undefined && html5len !== false) {
+                $.extend(defaults, {
+                    limit: parseInt($(this).attr('maxlength'))
+                });
+            }
+            if (!options.counterSelector) {
+                $(this).after(generateCounter());
+            }
             bindEvents(this);
             checkCount(this);
         });
-        
+
         function customFields(params)
         {
-            var html='';
+            var i, html='';
 
-            for (var i in params)
+            for (i in params)
             {
                 html += ' ' + i + '="' + params[i] + '"';
             }
@@ -52,52 +63,63 @@
         {
             var classString = options.counterCssClass;
 
-            if(options.customFields.class)
+            if ( options.customFields['class'] )
             {
-                classString += " " + options.customFields.class;
+                classString += " " + options.customFields['class'];
                 delete options.customFields['class'];
             }
-            
+
             return '<'+ options.counterWrapper +customFields(options.customFields)+' class="' + classString + '"></'+ options.counterWrapper +'>';
         }
 
         function renderText(count)
         {
-            return options.counterFormat.replace(/%1/, count);
+            var rendered_count = options.counterFormat.replace(/%1/, count);
+
+            if ( options.renderTotal )
+            {
+                rendered_count += '/'+ options.limit;
+            }
+
+            return rendered_count;
         }
 
         function checkCount(element)
         {
-            var characterCount  = $(element).val().length;
-            var remaining        = options.limit - characterCount;
+            var characterCount = $(element).val().length;
+            var counter = options.counterSelector ? $(options.counterSelector) : $(element).nextAll("." + options.counterCssClass).first();
+            var remaining = options.limit - characterCount;
+            var condition = remaining < 0;
 
-            if( remaining < 0 )
+            if ( options.increaseCounting )
             {
-                $(element).next("." + options.counterCssClass).addClass(options.counterExceededCssClass);
+                remaining = characterCount;
+                condition = remaining > options.limit;
+            }
+
+            if ( condition )
+            {
+                counter.addClass(options.counterExceededCssClass);
                 options.exceeded = true;
                 options.onExceed(characterCount);
             }
             else
             {
-                if(options.exceeded) {
-                    $(element).next("." + options.counterCssClass).removeClass(options.counterExceededCssClass);
+                if ( options.exceeded ) {
+                    counter.removeClass(options.counterExceededCssClass);
                     options.onDeceed(characterCount);
                     options.exceeded = false;
                 }
             }
 
-            $(element).next("." + options.counterCssClass).html(renderText(remaining));
-        };    
+            counter.html(renderText(remaining));
+        }
 
         function bindEvents(element)
         {
             $(element)
-                .bind("keyup", function () { 
-                    checkCount(element); 
-                })
-                .bind("paste", function () { 
-                    var self = this;
-                    setTimeout(function () { checkCount(self); }, 0);
+                .on("input change", function () {
+                    checkCount(element);
                 });
         }
     };
